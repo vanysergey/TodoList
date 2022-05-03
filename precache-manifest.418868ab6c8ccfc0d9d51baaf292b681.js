5f717835b3cbf3b1848ac39054c79d02677e0024
Sergey Vaniukou <vanysergey89@gmail.com>
2022-05-03 15:55:40 +0300

    Updates

---
+++ precache-manifest.418868ab6c8ccfc0d9d51baaf292b681.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3165b9ef07f10d475bb2bb0be4db9634",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "d0d44a0b59dae8ba0838",
+    "url": "/TodoList/static/css/main.3e143f03.chunk.css"
+  },
+  {
+    "revision": "ab51504f11ea965eaa59",
+    "url": "/TodoList/static/js/2.95fd6b4c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TodoList/static/js/2.95fd6b4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d44a0b59dae8ba0838",
+    "url": "/TodoList/static/js/main.dcfffaa2.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
